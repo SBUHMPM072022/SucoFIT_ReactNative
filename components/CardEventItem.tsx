895f1421fda5212035a45201d4ec8320e782b9b8
ivanfabriano <ivanfabrianos@gmail.com>
2024-08-28 10:45:40 +0700

    Finish UI home page

--- components/CardEventItem.tsx
+++ components/CardEventItem.tsx
@@ -1,5 +1,4 @@
-import { Text, View, StyleSheet } from "react-native"
-import { IoniconTemplate } from "./navigation/TabBarIcon"
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native"
 import { LinearGradient } from 'expo-linear-gradient';
 
 export const CardEventItem = () => {
@@ -11,7 +10,24 @@ export const CardEventItem = () => {
               start={{ x: 0, y: 1 }}
               end={{ x: 1, y: 0 }}
             >
-              <Text style={styles.text}>Hello, World!</Text>
+                <View style={styles.container}>
+                    <View style={styles.text_section}>
+                        <Text style={styles.title}>Event Marathon Sucofindo</Text>
+                        <Text style={styles.text_thin}>Sucofindo - GBK</Text>
+                        <View style={{ marginVertical: 5 }}>
+                            <Text style={styles.text_light}>Held On</Text>
+                            <Text style={styles.text_light}>20 July 2024</Text>
+                        </View>
+                        <View style={{ marginVertical: 7 }}>
+                            <TouchableOpacity
+                                style={styles.button_orange}
+                                onPress={() => alert('Custom Button ditekan!')}
+                            >
+                                <Text style={styles.button_text} >Join Event</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
             </LinearGradient>
       </View>
     )
@@ -21,18 +37,28 @@ const styles = StyleSheet.create({
     container: {
         flexDirection: 'row'
     },
-    gradient: {
-        flex: 1
+    card_container: {
+        flexDirection: 'row'
     },
-    text: {
+    gradient: {
+        flex: 1,
+        borderRadius: 10
+    },
+    text_thin: {
         color: '#fff',
-        fontSize: 20,
-        fontWeight: 'bold',
+        fontSize: 14,
+        fontWeight: '200'
+    },
+    text_light: {
+        color: '#fff',
+        fontSize: 14,
+        fontWeight: '300'
     },
     title: {
         fontFamily: 'PoppinsMedium', 
-        color: '#0077B7',
-        fontSize: 16
+        color: '#fff',
+        fontSize: 20,
+        fontWeight: 'bold',
     },
     subTitle: {
         fontSize: 18,
@@ -45,4 +71,18 @@ const styles = StyleSheet.create({
         backgroundColor: '#D8D8D8',
         marginHorizontal: 10,
     },
+    text_section: {
+        width: '50%',
+        padding: 15
+    },
+    button_orange: {
+        backgroundColor: '#FF7F3E',
+        borderRadius: 12
+    },
+    button_text: {
+        color: '#FFFFFF',
+        textAlign: 'center',
+        marginVertical: 5,
+        fontSize: 18
+    }
   });
